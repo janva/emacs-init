99a452eb32715ead4e3f3fede59dbc0a88d5068a
Janne Väisänen <janva415@student.liu.se>
2022-01-30 12:45:20 +0200

    Add documentation and reorder in small iterations.

--- init.el
+++ init.el
@@ -2,10 +2,11 @@
 (require 'package)
 
 (setq package-archives '(("melpa" ."https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
+
 (unless package-archive-contents
  (package-refresh-contents)) 
 
@@ -16,39 +17,38 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(setq inhibit-startup-message t)
-
-    (scroll-bar-mode -1)        ; Disable visible scrollbar
-    (tool-bar-mode -1)          ; Disable the toolbar
-    (tooltip-mode -1)           ; Disable tooltips
-    (set-fringe-mode 10)        ; Give some breathing room
-
-    (menu-bar-mode -1)           ; Disable the menu bar
-;; TODO Move these to some ohter block 
-    ;; Set up the visible bell
-    (setq visible-bell t)
-
-    ;; Make ESC quit prompts
+;; Make ESC quit prompts
     (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)         
+(tooltip-mode -1)          
+(set-fringe-mode 10)       
+
+(menu-bar-mode -1)         
+
+;; Set up the visible bell
+(setq visible-bell t)
+
 (use-package doom-themes
-       :init (load-theme 'doom-horizon t))
-     ;;use refresh-pakcages to get this working
-     ;; requires all-the-icons-install-fonts
-     (use-package all-the-icons)
-     (use-package doom-modeline
-       :ensure t
-       :init (doom-modeline-mode 1)
-       :custom ((doom-modeline-height 15)))
+  :init (load-theme 'doom-horizon t))
+;;use refresh-pakcages to get this working
+(use-package all-the-icons)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
-;;(column-number-mode)
-;;(global-display-line-numbers-mode t)
+(column-number-mode)
+;; (global-display-line-numbers-mode t)
 
-(dolist (mode'(org-mode-hook
-	       term-mode-hook
-	       shell-mode-hook
-	  eshell-mode-hook))
-  (add-hook mode (lambda() (display-line-numbers-mode 0))))
+     (dolist (mode'(org-mode-hook
+		    term-mode-hook
+		    shell-mode-hook
+	       eshell-mode-hook))
+       (add-hook mode (lambda() (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 170)
 
