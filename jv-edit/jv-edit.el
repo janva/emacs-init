2a714bf4b2ae9be6f93cbdf76d0a5f05fb3354a6
Janne Väisänen <janva415@student.liu.se>
2022-03-08 11:10:44 +0200

    Add begin editing commands.

--- jv-edit/jv-edit.el
+++ jv-edit/jv-edit.el
@@ -1,25 +1,48 @@
+
+
+;; maybe refactor...
+(defun copy-line ()
+"Copy line(s) into kill-ring. "
+  (let ((beg (line-beginning-position))
+	(end (line-end-position)))
+    (save-excursion 
+    (when mark-active
+     (if (> (point) (mark))
+ 	 (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+       (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+     (copy-region-as-kill beg end)
+   )))
+
+
 (defun duplicate--line (&optional direction)
-  "Duplicates line of text in DIRECTION. 1 will duplicate to line bellow and -1 to line abbove current line. default value is 1"
-  (message"Under construction"))
-(duplicate--line)
+  "Duplicates line(s) of text in DIRECTION. if DIRECTION is 1  duplicate to line bellow else duplicate to line abbove current line." 
+  (save-mark-and-excursion
+(copy-line)
+(open-newline)
+  (yank))
+  (when (eq direction 1)
+    (next-line)))
 
+;;Fixme only works once for selected region since loosing the marked area when doing next line
 (defun duplicate-line-down  ()
-  "Creates a newline containing content of current line below the current line. "
+  "Creates newline(s) containing content of current line(s) below the current line. "
   (interactive)
-  (save-excursion 
-    (beginning-of-line)
-    (push-mark)
-    (end-of-line)
-    (copy-region-as-kill (point) (mark))
-    (newline)
-    (yank))
-  (next-line))
+  (duplicate--line 1))
 
 
+(defun duplicate-line-up  ()
+  "Creates  newline(s) containing content of current line(s) above the current line. "
+  (interactive)
+    (duplicate--line))
 
+(defun open-newline (&optional n)
+  "Opens a new line below current line even if cursor is in middle of current line.Move point to opened line. If N is set open n lines."
+  (interactive "pNumber of lines to open: ")
+  (goto-char (line-end-position))
+	     (newline (or N 1)))
 
-
-
-
-
+(defun move-line-down ()
+  "Moves whole line down"
+  (interactive "P")
+  )
 
