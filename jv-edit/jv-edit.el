be42b8c547b7896c29f88779626a8606ba41afb4
Janne Väisänen <janva415@student.liu.se>
2022-03-26 19:41:46 +0200

    Make it a  minor-mode and package.

--- jv-edit/jv-edit.el
+++ jv-edit/jv-edit.el
@@ -1,28 +1,66 @@
+;;; jv-edit.el --- Some basic text editing commands  -*- lexical-binding:t; -*-
+
+
+;; Author: Janne Väisänen <janva415@gmail.com>
+;; Created 26 Mar 2022
+;; Version: 0.1
+
+;; Keywords: elisp, textediting
+;; URL:
+
+;;; Commentary
+
+;; This package provides a minor mode jv-basic-edit with some basic text editing
+;; commands such as copy whole line.  This file is not part of GNU Emacs.
+
+;; This file is free sofware...
+
+;;;###autoload 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;; Minor mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; To create a minor mode we need to have
 ;; a variable ending with -mode
 ;; a function with same name as variable
-;;if we want it to be local to certain buffer set make-variable-buffer-localm
+;; if we want it to be local to certain buffer set make-variable-buffer-localm
 
-;; we can in addtition enable keymaps by adding entries in minor-mode-map-alist. should be pairs of minor modes symbol and keymap to be used
+;;  In addtition we can enable keymaps by adding entries in minor-mode-map-alist.
+;; should be pairs of minor modes symbol and keymap to be used
 
 ;;we can alsp enable lighter for minor-mode-alist
 ;; again usin minor mode variable symbol and
 
-(make-variable-buffer-local
- (defvar jv-basic-edit-mode nil)
- "Toggle jv-edit-basic-mode")
+;; If we want to use hooks we then we can add it a variable initialized to nil
+;; named modename-mode-hook. running the hooks can be done using
+
+;; (make-variable-buffer-local
+
+;;; Code:
+;; (
+(defvar jv-basic-edit-mode nil "Toggle jv-edit-basic-mode")
+;; )
 
 (defvar jv-basic-edit-mode-map (make-sparse-keymap) "The keymap for jv-edit-basic-mode")
 
-(define-key jv-basic-edit-mode-map (kbd "C-<return>" 'open-new-line ))
+;; Hooks on attached to this list will be run whenever mode is activated/deactivated
+(defvar jv-basic-edit-mode-hook nil "The hook for jv-basic-edit-mode")
+
+(define-key jv-basic-edit-mode-map (kbd "C-<return>") 'open-new-line )
+(define-key jv-basic-edit-mode-map (kbd "M-S-<down>") 'duplicate-line-down )
+(define-key jv-basic-edit-mode-map (kbd "M-S-<up>" ) 'duplicate-line-up)
+(define-key jv-basic-edit-mode-map (kbd "M-<down>")'swap-line-down )
+(define-key jv-basic-edit-mode-map (kbd "M-<up>")  'swap-line-up )
+
 ;;register minor mode
 (add-to-list 'minor-mode-alist '(jv-basic-edit-mode " jv-edit"))
-;;register keybingings map
-(add-to-list 'minor-mode-alist '(jv-basic-edit-mode jv-basic-edit-mode-map))
+
+;;register keybingings map (associate with this mode) 
+(add-to-list 'minor-mode-map-alist ( cons 'jv-basic-edit-mode jv-basic-edit-mode-map))
 
 (defun jv-basic-edit-mode (&optional ARG)
   "jv-basic-edit-mode is a minor mode consisting of a few basic editing commands. If ARG positive number > 0  activate mode else deactivate.If ARG is 'toggle then toggle mode"
   (interactive (list 'toggle))
-  (setq jv-basic-mode
+  (setq jv-basic-edit-mode
 	(if (eq ARG 'toggle)
 	    (not jv-basic-edit-mode)
 	  (> ARG 0)))
@@ -30,6 +68,9 @@
       (message "jv-basic-mode activated")
     (message "jv-basic-mode deactivated"))
   (run-hooks 'jv-basic-edit-mode-hook))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun open-newline (&optional n)
   "Opens a new line below current line even if cursor is in middle of current line.Move point to opened line. If N is set open n lines."
@@ -81,6 +122,7 @@
   (save-excursion 
     (insert  (substring  (current-kill 0 ) 0 -1))))
 
+;;FIXME under costruction
 (defun swapline-up (arg)
   "Swaps current line with previous line above."
   (interactive "P")
@@ -89,65 +131,13 @@
   (save-excursion
     (insert
      (current-kill 0))))
-
-;;hmmm;;hmmm
-;; more
+(provide 'jv-edit)
+;;; jv-edit.el ends here
+;; (current-kill 0))
 ;; (barf-if-buffer-read-only)
 ;; (barf-if-buffer-read-only)
-(insert
-(current-kill 0));; (barf-if-buffer-read-only)
+;; (barf-if-buffer-read-only)
 
 
 
 
-;;;;; just some stuff from eintr ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun mark-whole-buffer ( )
-  "put point at beginning and mark at end of buffer"
-  (interactive)
-  (goto-char(point-max))    
-  (push-mark(point))
-  (goto-char(point-min))
-  )
-
-
-(defun display-60-first-chars ()
-  "displays 60 first character in buffer even if buffer is narrowed"
-  (interactive )
-  (save-excursion
-    (save-restriction
-      (widen)
-      (message "%s" (buffer-substring (point-min) 60)))))
-
-(defun test-search (string)
-  "search for for STRING in forward direction inside current buffer. Set point after string if found else error. "
-  (interactive "sString of text to searh for:")
-  (search-forward string)
-  )
-
-(defun thrid-el-in-kill-ring ()
-  "Print value of third element in kill ring if exists in minibuffer."
-  (interactive )
-  (if (>(length kill-ring) 2 )
-      (nth 3 kill-ring )
-    (message "Kill ring does not contain 3 elements.")  
-)
-  )
-(forward-line 1)
-
-dskaölfkfd yank
-dsfasfdsdf
-sdfasa
-
-;;(buffer-substring)
-;; (filter-buffer-substring)
-;; is replacement for buffer-substring it returns substring whitout certain text properties
-
-;; (condition-case)
-;; is elisp exception handeling mechanism similar a bit like try catch 
-
-;; (save-restriction)
-;; is similiar to save-excursion but it reset narrowing.
-;; (condition-case
-;;     (var-part)
-;;     (body-part)
-;;  (error-part))
