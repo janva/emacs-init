7d8dcd109d36b9ece83ad51f2d109a7bb1f81157
Janne Väisänen <janva415@gmail.com>
2022-07-29 22:55:23 +0300

    Add start on org-captures.

--- init.el
+++ init.el
@@ -55,6 +55,11 @@
                    (interactive) 
                    (find-file "~/.config/emacs/emacs.org")))
 
+(defcustom jv-agenda-directory "~/programering/emacs/org-agenda/" 
+"Base directory of my agenda files"
+:type 'string
+:options '("~/programering/emacs/org-agenda/" ))
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
@@ -96,6 +101,12 @@
 
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height 210 :weight 'regular)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (or (daemonp) (memq window-system '(ns x)))
+    (exec-path-from-shell-initialize)))
 
 (use-package swiper
      :ensure t)
@@ -206,23 +217,140 @@
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
 
+(setq org-agenda-files '( 
+                             "~/programering/emacs/org-agenda/Birthdays.org"
+                             "~/programering/emacs/org-agenda/Archive.org"))
+(cons (expand-file-name  "Tasks.org" jv-agenda-directory) org-agenda-files)
+    (setq org-agenda-start-with-log-mode t)
+    (setq org-log-done 'time)
+    (setq org-log-into-drawer t)
+
+(setq org-todo-keywords  
+      '((sequence  "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+        (sequence  "BACKLOG(b)" "PLAN(p)"  "READY(r)"
+                   "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" 
+                   "|" "COMPLETED(c)" "CANCEL(k@)")))
+
+(setq org-refile-targets
+  '(("Archive.org" :maxlevel . 1)
+    ("Tasks.org" :maxlevel . 1)))
+;; Save Org buffers after refiling!
+(advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+(setq org-tag-alist
+    '((:startgroup)
+       ; Put mutually exclusive tags here
+       (:endgroup)
+       ("@errand" . ?E)
+       ("@home" . ?H)
+       ("@work" . ?W)
+       ("agenda" . ?a)
+       ("planning" . ?p)
+       ("publish" . ?P)
+       ("batch" . ?b)
+       ("note" . ?n)
+       ("idea" . ?i)))
+
+;; Configure custom agenda views
+(setq org-agenda-custom-commands
+ '(("d" "Dashboard"
+   ((agenda "" ((org-deadline-warning-days 7)))
+    (todo "NEXT"
+      ((org-agenda-overriding-header "Next Tasks")))
+    (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+  ("n" "Next Tasks"
+   ((todo "NEXT"
+      ((org-agenda-overriding-header "Next Tasks")))))
+
+  ("W" "Work Tasks" tags-todo "+work-email")
+
+  ;; Low-effort next actions
+  ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+   ((org-agenda-overriding-header "Low Effort Tasks")
+    (org-agenda-max-todos 20)
+    (org-agenda-files org-agenda-files)))
+
+  ("w" "Workflow Status"
+   ((todo "WAIT"
+          ((org-agenda-overriding-header "Waiting on External")
+           (org-agenda-files org-agenda-files)))
+    (todo "REVIEW"
+          ((org-agenda-overriding-header "In Review")
+           (org-agenda-files org-agenda-files)))
+    (todo "PLAN"
+          ((org-agenda-overriding-header "In Planning")
+           (org-agenda-todo-list-sublevels nil)
+           (org-agenda-files org-agenda-files)))
+    (todo "BACKLOG"
+          ((org-agenda-overriding-header "Project Backlog")
+           (org-agenda-todo-list-sublevels nil)
+           (org-agenda-files org-agenda-files)))
+    (todo "READY"
+          ((org-agenda-overriding-header "Ready for Work")
+           (org-agenda-files org-agenda-files)))
+    (todo "ACTIVE"
+          ((org-agenda-overriding-header "Active Projects")
+           (org-agenda-files org-agenda-files)))
+    (todo "COMPLETED"
+          ((org-agenda-overriding-header "Completed Projects")
+           (org-agenda-files org-agenda-files)))
+    (todo "CANC"
+          ((org-agenda-overriding-header "Cancelled Projects")
+           (org-agenda-files org-agenda-files)))))))
+
+     "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+
+(setq org-capture-templates
+    `(("t" "Tasks / Projects")
+      ("tt" "Task" entry (file+olp "~/programering/emacs/org-agenda/Tasks.org" "Inbox")
+           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+      ("j" "Journal Entries")
+      ("jj" "Journal" entry
+           (file+olp+datetree "~/Projects/Code/emacs-from-scratch/OrgFiles/Journal.org")
+           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+           :clock-in :clock-resume
+           :empty-lines 1)
+      ("jm" "Meeting" entry
+           (file+olp+datetree "~/Projects/Code/emacs-from-scratch/OrgFiles/Journal.org")
+           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
+
+      ("w" "Workflows")
+      ("we" "Checking Email" entry (file+olp+datetree "~/Projects/Code/emacs-from-scratch/OrgFiles/Journal.org")
+           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
+
+      ("m" "Metrics Capture")
+      ("mw" "Weight" table-line (file+headline "~/Projects/Code/emacs-from-scratch/OrgFiles/Metrics.org" "Weight")))
+    
+(define-key global-map (kbd "C-c j")
+    (lambda () (interactive) (org-capture nil "jj")))
+
 (org-babel-do-load-languages
 'org-babel-do-load-languages '(
 (emacs-lisp . t)
+(java . t)
+(javascript. t)
 (python . t)))
 
 (require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
+      (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+      (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+      (add-to-list 'org-structure-template-alist '("py" . "src python"))
+      ( add-to-list 'org-structure-template-alist ' ("java"."src java"))
+( add-to-list 'org-structure-template-alist ' ("javascript"."src javascript"))
+      (add-to-list 'org-structure-template-alist '("xml" . "src xml"))
 
 ;;auto-tangle files to target on save
 (defun efs/org-babel-tangle-config ()
   (when (string-equal (buffer-file-name)
-(expand-file-name ".emacs.org"))
+(expand-file-name "emacs.org"))
 ;; Dynamic scoping to the rescue
 (let ((org-confirm-babel-evaluate nil))
-(org-babel-tangle))))
+  (org-babel-tangle))))
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
