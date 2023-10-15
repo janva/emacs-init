dab8cb478936ed0382fad52efc0b21f4b09c8d4b
Janne Väisänen <janva415@gmail.com>
2023-10-15 18:39:21 +0300

    Add cmake lsp server settings.

--- init.el
+++ init.el
@@ -587,40 +587,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" default))
- '(exwm-floating-border-color "#16161c")
- '(fci-rule-color "#f9cec3")
- '(highlight-tail-colors ((("#203a3b") . 0) (("#283841") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#16161c" "#e95678"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#16161c" "#09f7a0"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#16161c" "#6a6a6a"))
- '(objed-cursor-color "#e95678")
  '(package-selected-packages
-   '(visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package))
- '(pdf-view-midnight-colors (cons "#c7c9cb" "#232530"))
- '(rustic-ansi-faces
-   ["#232530" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
- '(vc-annotate-background "#232530")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#09f7a0")
-    (cons 40 "#59e19c")
-    (cons 60 "#a9cc98")
-    (cons 80 "#fab795")
-    (cons 100 "#f6ab8f")
-    (cons 120 "#f39f89")
-    (cons 140 "#f09383")
-    (cons 160 "#c48788")
-    (cons 180 "#987a8d")
-    (cons 200 "#6c6f93")
-    (cons 220 "#95668a")
-    (cons 240 "#bf5e81")
-    (cons 260 "#e95678")
-    (cons 280 "#c95b74")
-    (cons 300 "#a96071")
-    (cons 320 "#89656d")
-    (cons 340 "#f9cec3")
-    (cons 360 "#f9cec3")))
- '(vc-annotate-very-old-color nil))
+   '(cmake-font-lock cmake-mode visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
