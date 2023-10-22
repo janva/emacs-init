7e5040fe4e32243ccba34aec0b9bd8de31fcf99d
Janne <janva415@gmail.com>
2023-10-22 17:02:27 +0300

    Fix c++ mode.

--- init.el
+++ init.el
@@ -468,6 +468,21 @@
 (add-to-list 'major-mode-remap-alist '(c++-mode. c++-ts-mode))
 (add-to-list 'major-mode-remap-alist '(c-or-c++-mode. c-or-c++-ts-mode))
 
+(use-package cmake-mode
+  :ensure t
+  :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'")
+  :hook (cmake-mode . lsp-deferred))
+
+(use-package cmake-font-lock
+  :ensure t
+  :after cmake-mode
+  :config (cmake-font-lock-activate))
+
+(use-package c++-mode 
+:ensure nil
+:mode ("\\.cpp\\'" "\\.cc\\'" "\\.h\\'")
+:hook(c++-mode .lsp))
+
 (use-package typescript-mode
 :mode "\\.ts\\'"
 :hook (typescript-mode . lsp-deferred)
@@ -587,8 +602,15 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" default))
+ '(exwm-floating-border-color "#16161c")
+ '(fci-rule-color "#f9cec3")
+ '(highlight-tail-colors ((("#203a3b") . 0) (("#283841") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#16161c" "#e95678"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#16161c" "#09f7a0"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#16161c" "#6a6a6a"))
+ '(objed-cursor-color "#e95678")
  '(package-selected-packages
-   '(markdown-toc visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package))
+   '(visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package))
  '(pdf-view-midnight-colors (cons "#c7c9cb" "#232530"))
  '(rustic-ansi-faces
    ["#232530" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
@@ -614,7 +636,6 @@
     (cons 340 "#f9cec3")
     (cons 360 "#f9cec3")))
  '(vc-annotate-very-old-color nil))
-   '(cmake-font-lock cmake-mode visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
