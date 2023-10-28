f4657c308ce4b286babea80ccbb021162da8337c
Janne <janva415@gmail.com>
2023-10-28 19:45:32 +0300

    Add Migrate to using straight package manager amongst other things.

--- init.el
+++ init.el
@@ -1,30 +1,51 @@
+;; Silence compiler warnings as they can be pretty disruptive
+(setq comp-async-report-warnings-errors nil)
+
+;; seems to start daemon by default so no need?
+;; (server-start)
+
 ;; Initialize package sources
- (require 'package)
+;;          (require 'package)
+;; 
+;;         ;; Note org elpa will close before 9.6 use org gnu instead.
+;;         (setq package-archives '(("melpa" ."https://melpa.org/packages/")
+;;                                  ;; seems this repo is closing
+;;                                  ;;("org" . "https://elpa.gnu.org/packages/")
+;;                                  ("nongnu". "https://elpa.nongnu.org/nongnu/")
+;;                                  ("elpa" . "https://elpa.gnu.org/packages/")))
+;; 
+;;          (package-initialize)
+;; 
+;;          (unless package-archive-contents
+;;           (package-refresh-contents)) 
+;; 
+      ;; Initialize use-package on non-Linux platforms
+      ;;       (unless (package-installed-p 'use-package)
+      ;;          (package-install 'use-package))
+      ;;
+      ;;       (require 'use-package)
+      ;;       (setq use-package-always-ensure t)
 
-;; Note org elpa will close before 9.6 use org gnu instead.
-(setq package-archives '(("melpa" ."https://melpa.org/packages/")
-                         ;; seems this repo is closing
-                         ;;("org" . "https://elpa.gnu.org/packages/")
-                         ("nongnu". "https://elpa.nongnu.org/nongnu/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
- (package-initialize)
-
- (unless package-archive-contents
-  (package-refresh-contents)) 
- ;; Initialize use-package on non-Linux platforms
- (unless (package-installed-p 'use-package)
-    (package-install 'use-package))
-
- (require 'use-package)
- (setq use-package-always-ensure t)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+(require 'straight-x)
 
 (global-visual-line-mode  1)
 
 (setq scroll-conservatively 99)
-
-(use-package jv-basic-edit
-  :config (jv-basic-edit-mode 1))
 
 ;; get to agen faster
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -78,18 +99,19 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(require 'all-the-icons)
-             (use-package doom-themes
-               :init (load-theme 'doom-horizon t))
-             ;;use refresh-pakcages to get this working
-     ;; M-x all-the-icons-install-fonts
-             (use-package all-the-icons
-               :if (display-graphic-p)
-               )
-             (use-package doom-modeline
-               :ensure t
-               :init (doom-modeline-mode 1)
-               :custom ((doom-modeline-height 15)))
+(use-package all-the-icons
+  :straight t
+  :if (display-graphic-p))
+
+
+(use-package doom-themes
+  :init (load-theme 'doom-horizon t))
+;;use refresh-pakcages to get this working
+;; M-x all-the-icons-install-fonts
+(use-package doom-modeline
+  :straight t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 (column-number-mode)
 
@@ -109,7 +131,7 @@
 (set-face-attribute 'variable-pitch nil :font "Cantarell" :height 210 :weight 'regular)
 
 (use-package exec-path-from-shell
-  :ensure t
+  :straight t
   :config
   (when (or (daemonp) (memq window-system '(ns x)))
     (exec-path-from-shell-initialize)))
@@ -117,23 +139,23 @@
 (add-hook 'flyspell-mode-hook (lambda () (local-set-key (kbd "C-.") #'flyspell-correct-word-before-point )))
 
 (use-package swiper
-     :ensure t)
+     :straight t)
 
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)	
-	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
-	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-l" . ivy-done)
-	 ("C-d" . ivy-switch-buffer-kill)
-	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
+	      :map ivy-minibuffer-map
+	      ("TAB" . ivy-alt-done)	
+	      ("C-l" . ivy-alt-done)
+	      ("C-j" . ivy-next-line)
+	      ("C-k" . ivy-previous-line)
+	      :map ivy-switch-buffer-map
+	      ("C-k" . ivy-previous-line)
+	      ("C-l" . ivy-done)
+	      ("C-d" . ivy-switch-buffer-kill)
+	      :map ivy-reverse-i-search-map
+	      ("C-k" . ivy-previous-line)
+	      ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
 
@@ -141,10 +163,10 @@
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history))
+	      ("C-x b" . counsel-ibuffer)
+	      ("C-x C-f" . counsel-find-file)
+	      :map minibuffer-local-map
+	      ("C-r" . counsel-minibuffer-history))
   :config
   (setq ivy-initial-alist nil )) ; don't start search with ^
 
@@ -170,11 +192,11 @@
   ("f" nil "finnished" :exit t))
 
 (use-package  tex
-    :ensure auctex
+    :straight auctex
     :hook (LaTeX-mode .  (lambda ()
-			   (setq TeX-auto-save t)
-			   (set TeX-parse-self t)
-			   (set-default TeX-master nil)))
+			     (setq TeX-auto-save t)
+			     (set TeX-parse-self t)
+			     (set-default TeX-master nil)))
     :config
     (setq TeX-PDF-mode t)
     (setq TeX-view-program-selection '((output-pdf "PDF Tools")))
@@ -188,49 +210,49 @@
 (setq pdf-view-resize-factor 1.1))
   ;; keybindings   
   (use-package latex
-    :ensure auctex
+    :straight auctex
     :bind (:map LaTeX-mode-map
-		("C-c C-c". TeX-command-run-all)))
+		  ("C-c C-c". TeX-command-run-all)))
 
 ;; syntax highlight 
 (add-hook 'LaTeX-mode-hook 'turn-on-font-lock)
 
 (defun efs/org-font-setup ()
 ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-			  '(("^ *\\([-]\\) "
-			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+	(font-lock-add-keywords 'org-mode
+				'(("^ *\\([-]\\) "
+				   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-		  (org-level-2 . 1.1)
-		  (org-level-3 . 1.05)
-		  (org-level-4 . 1.0)
-		  (org-level-5 . 1.1)
-		  (org-level-6 . 1.1)
-		  (org-level-7 . 1.1)
-		  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+	;; Set faces for heading levels
+	(dolist (face '((org-level-1 . 1.2)
+			(org-level-2 . 1.1)
+			(org-level-3 . 1.05)
+			(org-level-4 . 1.0)
+			(org-level-5 . 1.1)
+			(org-level-6 . 1.1)
+			(org-level-7 . 1.1)
+			(org-level-8 . 1.1)))
+	  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
 ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+	(set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+	(set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+	(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+	(set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+	(set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+	(set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
 
 (use-package org
-  :hook (org-mode . efs/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾" 
-	org-hide-emphasis-markers t
-	org-src-tab-acts-natively t))
+	:hook (org-mode . efs/org-mode-setup)
+	:config
+	(setq org-ellipsis " ▾" 
+	      org-hide-emphasis-markers t
+	      org-src-tab-acts-natively t))
 
 (defun efs/org-mode-setup()
   (org-indent-mode)
@@ -239,15 +261,15 @@
   (visual-line-mode 1))
 
 (use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+	:after org
+	:hook (org-mode . org-bullets-mode)
+	:custom
+	(org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-	visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
+	(setq visual-fill-column-width 100
+	      visual-fill-column-center-text t)
+	(visual-fill-column-mode 1))
 
 (use-package visual-fill-column
   :hook (org-mode . efs/org-mode-visual-fill))
@@ -382,7 +404,7 @@
    :hook (org-mode . org-fragtog-mode)
    :config
   (setq org-format-latex-options
-      (plist-put org-format-latex-options :scale 2.0)))
+	(plist-put org-format-latex-options :scale 2.0)))
 
 (org-babel-do-load-languages
 'org-babel-do-load-languages '(
@@ -413,7 +435,8 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package prog-mode
-  :ensure nil
+  :straight nil
+  ;; :hook (prog-mode . lsp-deferred)
   :init (show-paren-mode  t)
   (electric-pair-mode 1  ))
 
@@ -432,7 +455,8 @@
 
 (use-package lsp-mode
 :commands (lsp lsp-deferred)
-:hook (lsp-mode . efs/lsp-mode-setup)
+:hook ((lsp-mode . efs/lsp-mode-setup)
+       (c++-mode.lsp-defferd))
 :init
 (setq lsp-keymap-prefix "C-c l")  
 :config
@@ -451,37 +475,79 @@
 
 (use-package lsp-ivy)
 
-(setq treesit-language-source-alist
-    '((cpp  "https://github.com/tree-sitter/tree-sitter-cpp")
-    (c  "https://github.com/tree-sitter/tree-sitter-c")))
+(use-package treesit
+    :straight (:type built-in)
+    :if  (featurep 'treesit)
+    :config
+    (setq treesit-language-source-alist
+          '((cpp  "https://github.com/tree-sitter/tree-sitter-cpp")
+            (c  "https://github.com/tree-sitter/tree-sitter-c")
+  	    (bash "https://github.com/tree-sitter/tree-sitter-bash")
+    	    (cmake "https://github.com/uyha/tree-sitter-cmake")
+    	    (css "https://github.com/tree-sitter/tree-sitter-css")
+    	    (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+    	    (go "https://github.com/tree-sitter/tree-sitter-go")
+    	    (html "https://github.com/tree-sitter/tree-sitter-html")
+    	    (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+    	    (json "https://github.com/tree-sitter/tree-sitter-json")
+    	    (make "https://github.com/alemuller/tree-sitter-make")
+    	    (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+    	    (python "https://github.com/tree-sitter/tree-sitter-python")
+    	    (toml "https://github.com/tree-sitter/tree-sitter-toml")
+    	    (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+    	    (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+    	    (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
-(dolist (lang treesit-language-source-alist)
-  (unless (treesit-language-available-p (car lang))
-  (treesit-install-language-grammar (car lang))))
+    (setq treesit-font-lock-level 4)
 
-;;treesitter mode for c++ is the c++-ts-mode lets remap c++ to this name
-(setq treesit-load-name-override-list
-      '((c++ "libtree-sitter-cpp")))
+    (dolist (lang treesit-language-source-alist)
+      (unless (treesit-language-available-p (car lang))
+        (treesit-install-language-grammar (car lang))))
 
-;; Lets use tree-sitter  as default mode for c++
-(add-to-list 'major-mode-remap-alist '(c-mode. c-ts-mode))
-(add-to-list 'major-mode-remap-alist '(c++-mode. c++-ts-mode))
-(add-to-list 'major-mode-remap-alist '(c-or-c++-mode. c-or-c++-ts-mode))
+    ;;treesitter mode for c++ is the c++-ts-mode lets remap c++ to this name
+    
+    (setq treesit-load-name-override-list
+          '((c "libtree-sitter-c")
+	    (c++ "libtree-sitter-cpp")))
+
+
+  ;; Lets use tree-sitter  as default mode for c++
+(setq major-mode-remap-alist
+	'( (typescript-mode . typescript-ts-mode)
+	  (js-mode . javascript-ts-mode)
+	  (python-mode . python-ts-mode)
+	  (json-mode . json-ts-mode)))
+ 
+  (add-to-list 'major-mode-remap-alist '(c-mode. c-ts-mode))
+    (add-to-list 'major-mode-remap-alist '(c++-mode. c++-ts-mode))
+    (add-to-list 'major-mode-remap-alist '(c-or-c++-mode. c-or-c++-ts-mode)))
 
 (use-package cmake-mode
-  :ensure t
+  :straight t
   :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'")
   :hook (cmake-mode . lsp-deferred))
 
 (use-package cmake-font-lock
-  :ensure t
+  :straight t
   :after cmake-mode
   :config (cmake-font-lock-activate))
 
 (use-package c++-mode 
-:ensure nil
-:mode ("\\.cpp\\'" "\\.cc\\'" "\\.h\\'")
-:hook(c++-mode .lsp))
+:straight nil
+:mode "\\.cpp\\'"
+:mode "\\.cc\\'"
+:mode "\\.h\\'")
+;; :hook (c++-mode . lsp-deferred))
+
+(use-package lsp-java
+  :straight t
+  :after (lsp)
+  :config
+  (setq lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/milestones/0.57.0/jdt-language-server-0.57.0-202006172108.tar.gz"))
+
+(add-hook 'java-mode-hook #'smartparens-mode)
+;; (add-hook 'java-mode-hook #'hs-minor-mode)
+;; (my/set-smartparens-indent 'java-mode)
 
 (use-package typescript-mode
 :mode "\\.ts\\'"
@@ -508,7 +574,7 @@
   ;;  "d" '(dap-hydra t :wk "debugger")))
 
 (use-package python-mode
-  :ensure t
+  :straight t 
   :hook (python-mode . lsp-deferred)
   :custom
   ;; NOTE: Set these if Python 3 is called "python3" on your system!
@@ -525,23 +591,38 @@
 
 (use-package sh-mode
   :mode "\\.sh\\'"
-  :ensure nil
+  :straight nil
   :hook (sh-mode . lsp-deferred))
 
 (defun   jv/setup-emacs-lisp-mode()
    (message "running my hook")
 ;;     (push '(company-elisp :with company-yasnippet)  company-backends)
+   
      (setq-local  company-backends '((company-elisp :with company-yasnippet))))
 
+(setq company-global-modes nil)
+
+(use-package lisp
+  :hook after-save . check-parens)
+
+(use-package elisp-mode
+  :straight (:type built-in)
+  :hook (elisp-mode .company-mode
+		    'emacs-lisp-mode-hook .'jv/setup-emacs-lisp-mode))
+
 ;; TODO hmm would like to make a seperation as well that is use :separate
-(use-package emacs-lisp-mode
-  :ensure nil
-  :mode  "\\.el\\'"
-  :hook (emacs-lisp-mode . company-mode)
-  ;;company-elisp is obsolete?
-  ;; could just use push instead?
-  :custom  (company-backends    '(( company-yasnippet :separate company-capf company-dabbrev-code ))))
-                                          ;;  ( emacs-lisp-mode . jv/setup-emacs-lisp-mode) )
+
+   ;;  (use-package emacs-lisp
+   ;;   :straight (:type built-in) 
+   ;;   :commands emacs-lisp-mode
+   ;;   :mode  "\\.el\\'"
+   ;;   :hook (emacs-lisp-mode . company-mode)
+   ;;   ;;company-elisp is obsolete?
+   ;;   ;; could just use push instead?
+   ;;   :custom  (company-backends    '(( company-yasnippet :separate company-capf company-dabbrev-code ))))
+                                             ;;  ( emacs-lisp-mode . jv/setup-emacs-lisp-mode) )
+;;  (add-hook 'emacs-lisp-mode-hook #'jv/setup-emacs-lisp-mode)
+;;  (add-hook 'emacs-lisp-mode-hook 'company-mode)
 
 (use-package company
   :after lsp-mode
@@ -571,7 +652,7 @@
   ("C-c p". projectile-command-map)
   :init
   (when (file-directory-p "~/programering")
-    (setq projectile-project-search '("~programering")))
+	 (setq projectile-project-search '("~programering")))
   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package  counsel-projectile
@@ -594,51 +675,3 @@
   ([remap describe-key] . helpful-key))
 
 (use-package command-log-mode)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("23c806e34594a583ea5bbf5adf9a964afe4f28b4467d28777bcba0d35aa0872e" default))
- '(exwm-floating-border-color "#16161c")
- '(fci-rule-color "#f9cec3")
- '(highlight-tail-colors ((("#203a3b") . 0) (("#283841") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#16161c" "#e95678"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#16161c" "#09f7a0"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#16161c" "#6a6a6a"))
- '(objed-cursor-color "#e95678")
- '(package-selected-packages
-   '(visual-fill-column org-bullets magit counsel-projetile hydra helpful which-key doom-themes swiper doom-modeline ivy command-log-mode use-package))
- '(pdf-view-midnight-colors (cons "#c7c9cb" "#232530"))
- '(rustic-ansi-faces
-   ["#232530" "#e95678" "#09f7a0" "#fab795" "#21bfc2" "#6c6f93" "#59e3e3" "#c7c9cb"])
- '(vc-annotate-background "#232530")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#09f7a0")
-    (cons 40 "#59e19c")
-    (cons 60 "#a9cc98")
-    (cons 80 "#fab795")
-    (cons 100 "#f6ab8f")
-    (cons 120 "#f39f89")
-    (cons 140 "#f09383")
-    (cons 160 "#c48788")
-    (cons 180 "#987a8d")
-    (cons 200 "#6c6f93")
-    (cons 220 "#95668a")
-    (cons 240 "#bf5e81")
-    (cons 260 "#e95678")
-    (cons 280 "#c95b74")
-    (cons 300 "#a96071")
-    (cons 320 "#89656d")
-    (cons 340 "#f9cec3")
-    (cons 360 "#f9cec3")))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
